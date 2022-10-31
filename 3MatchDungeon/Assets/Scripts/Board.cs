a20ebf5050e3c559c9351991e588f2b053b307c1
wlsdnjs0707 <jinwon0707@gmail.com>
2022-11-01 01:02:09 +0900

    Add UI

--- 3MatchDungeon/Assets/Scripts/Board.cs
+++ 3MatchDungeon/Assets/Scripts/Board.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using DG.Tweening;
 using System;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public sealed class Board : MonoBehaviour
 {
@@ -16,6 +18,18 @@ public sealed class Board : MonoBehaviour
 
     public Row[] rows;
     public Tile[,] Tiles { get; private set; }
+
+    public GameObject gameEndUI;
+
+    public Button exitButton;
+
+    [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private TextMeshProUGUI coinText;
+
+    private int reward = 0;
+
+    private bool whilePopping = false;
+    private bool whileSwapping = false;
 
     // Dimension 0
     public int Width => Tiles.GetLength(0);
@@ -32,6 +46,8 @@ public sealed class Board : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        gameEndUI.SetActive(false);
+
         Tiles = new Tile[rows.Max(row => row.tiles.Length), rows.Length];
 
         for (var y=0; y < Height; y++)
@@ -58,26 +74,29 @@ public sealed class Board : MonoBehaviour
 
     public async void Select(Tile tile)
     {
-        if (!_selection.Contains(tile))
+        if (whilePopping == false && whileSwapping == false)
         {
-            // Second Tile Select
-            if(_selection.Count > 0)
+            if (!_selection.Contains(tile))
             {
-                // Select Adjacent Tile
-                if (Array.IndexOf(_selection[0].Neighbors,tile) != -1)
+                // Second Tile Select
+                if (_selection.Count > 0)
+                {
+                    // Select Adjacent Tile
+                    if (Array.IndexOf(_selection[0].Neighbors, tile) != -1)
+                    {
+                        _selection.Add(tile);
+                    }
+                    // Select Not Adjacent Tile
+                    else
+                    {
+                        _selection.RemoveAt(0);
+                    }
+                }
+                // First Tile Select
+                else
                 {
                     _selection.Add(tile);
                 }
-                // Select Not Adjacent Tile
-                else
-                {
-                    _selection.RemoveAt(0);
-                }
-            }
-            // First Tile Select
-            else
-            {
-                _selection.Add(tile);
             }
         }
 
@@ -98,33 +117,42 @@ public sealed class Board : MonoBehaviour
 
         MoveCountManager.Instance.Count -= 1;
 
+        MoveCountCheck();
+
         _selection.Clear();
     }
 
     public async Task Swap(Tile tile1, Tile tile2)
     {
-        var icon1 = tile1.icon;
-        var icon2 = tile2.icon;
+        if (whileSwapping == false)
+        {
+            whileSwapping = true;
 
-        var icon1Transform = icon1.transform;
-        var icon2Transform = icon2.transform;
+            var icon1 = tile1.icon;
+            var icon2 = tile2.icon;
 
-        var sequence = DOTween.Sequence();
+            var icon1Transform = icon1.transform;
+            var icon2Transform = icon2.transform;
 
-        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration)).Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
+            var sequence = DOTween.Sequence();
 
-        await sequence.Play().AsyncWaitForCompletion();
+            sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration)).Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
 
-        icon1Transform.SetParent(tile2.transform);
-        icon2Transform.SetParent(tile1.transform);
+            await sequence.Play().AsyncWaitForCompletion();
 
-        tile1.icon = icon2;
-        tile2.icon = icon1;
+            icon1Transform.SetParent(tile2.transform);
+            icon2Transform.SetParent(tile1.transform);
 
-        var tile1Item = tile1.Item;
+            tile1.icon = icon2;
+            tile2.icon = icon1;
 
-        tile1.Item = tile2.Item;
-        tile2.Item = tile1Item;
+            var tile1Item = tile1.Item;
+
+            tile1.Item = tile2.Item;
+            tile2.Item = tile1Item;
+
+            whileSwapping = false;
+        }
 
     }
 
@@ -146,47 +174,53 @@ public sealed class Board : MonoBehaviour
 
     private async void Pop()
     {
-        for (var y=0; y < Height; y++)
+        if (whilePopping == false)
         {
-            for (var x=0; x < Width; x++)
+            whilePopping = true;
+
+            for (var y = 0; y < Height; y++)
             {
-                var tile = Tiles[x, y];
-
-                var connectedTiles = tile.GetConnectedTiles();
-
-                if (connectedTiles.Skip(1).Count() < 2) continue;
-
-                // Damage Dealt
-                HealthManager.Instance.Health -= connectedTiles.Count;
-
-                // Play Sound Effect
-                _audioSource.PlayOneShot(collectSound);
-
-                // Deflate
-                var deflateSequence = DOTween.Sequence();
-                foreach (var connectedTile in connectedTiles)
+                for (var x = 0; x < Width; x++)
                 {
-                    deflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
-                }
-                await deflateSequence.Play().AsyncWaitForCompletion();
+                    var tile = Tiles[x, y];
 
-                // Inflate
-                var inflateSequence = DOTween.Sequence();
-                foreach ( var connectedTile in connectedTiles)
-                {
-                    connectedTile.Item = ItemDatabase.Items[UnityEngine.Random.Range(0, ItemDatabase.Items.Length)];
-                    inflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
-                }
-                await inflateSequence.Play().AsyncWaitForCompletion();
+                    var connectedTiles = tile.GetConnectedTiles();
 
-                // Re-Check
-                x = 0;
-                y = 0;
+                    if (connectedTiles.Skip(1).Count() < 2) continue;
+
+                    // Damage Dealt
+                    HealthManager.Instance.Health -= connectedTiles.Count;
+
+                    // Play Sound Effect
+                    _audioSource.PlayOneShot(collectSound);
+
+                    // Deflate
+                    var deflateSequence = DOTween.Sequence();
+                    foreach (var connectedTile in connectedTiles)
+                    {
+                        deflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
+                    }
+                    await deflateSequence.Play().AsyncWaitForCompletion();
+
+                    // Inflate
+                    var inflateSequence = DOTween.Sequence();
+                    foreach (var connectedTile in connectedTiles)
+                    {
+                        connectedTile.Item = ItemDatabase.Items[UnityEngine.Random.Range(0, ItemDatabase.Items.Length)];
+                        inflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
+                    }
+                    await inflateSequence.Play().AsyncWaitForCompletion();
+
+                    // Re-Check
+                    x = 0;
+                    y = 0;
+                }
             }
+
+            whilePopping = false;
         }
 
         HealthCheck();
-        MoveCountCheck();
     }
 
     private void HealthCheck()
@@ -195,34 +229,79 @@ public sealed class Board : MonoBehaviour
         if (HealthManager.Instance.Health <= 0)
         {
             // Win Event
+            gameEndUI.SetActive(true);
+
             if (SceneManager.GetActiveScene().name == "Stage1")
             {
                 GameManager.instance.stage2access = true;
-                GameManager.instance.coin += 100;
+                reward = 100;
             }
 
             if (SceneManager.GetActiveScene().name == "Stage2")
             {
                 GameManager.instance.stage3access = true;
-                GameManager.instance.coin += 500;
+                reward = 500;
             }
 
             if (SceneManager.GetActiveScene().name == "Stage3")
             {
-                GameManager.instance.coin += 5000;
                 // Ending
             }
 
-            SceneManager.LoadScene("Main");
+            statusText.SetText($"YOU WIN");
+            coinText.SetText($"{reward}");
+            GameManager.instance.coin += reward;
+
+            // Stop
+            Time.timeScale = 0.0f;
         }
     }
 
     private void MoveCountCheck()
     {
+        // Prevent Error
+        if (whilePopping == true || whileSwapping == true)
+        {
+            Invoke("MoveCountCheck", 1);
+            return;
+        }
+
         if (MoveCountManager.Instance.Count <= 0)
         {
+            // Disable Select
+            whilePopping = true;
+            whileSwapping = true;
+
             // Lose Event
-            SceneManager.LoadScene("Main");
+            gameEndUI.SetActive(true);
+
+            if (SceneManager.GetActiveScene().name == "Stage1")
+            {
+                reward = 50 - HealthManager.Instance.Health;
+            }
+
+            if (SceneManager.GetActiveScene().name == "Stage2")
+            {
+                reward = 150 - HealthManager.Instance.Health;
+            }
+
+            if (SceneManager.GetActiveScene().name == "Stage3")
+            {
+                reward = 300 - HealthManager.Instance.Health;
+            }
+
+            statusText.SetText($"YOU LOSE");
+            coinText.SetText($"{reward}");
+            GameManager.instance.coin += reward;
+
+            // Stop
+            Time.timeScale = 0.0f;
         }
+    }
+
+    public void ExitGame()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Main");
     }
 }
